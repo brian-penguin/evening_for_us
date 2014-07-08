dba7b874a0bc1f75aac4dbcddbde116089dd8bdb
brimatteng <btenggren20@gmail.com>
2014-07-08 14:51:22 -0400

    Add create function and redirecting to main pages

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -11,6 +11,16 @@ class NumbersController <ApplicationController
   end
 
   def create
+    @number = current_user.numbers.create(number_params)
+
+    if @number.save
+      flash[:notice] = 'Number added!'
+      redirect_to root_path
+    else
+      flash.now[:alert] = @number.errors.full_messages.join(', ')
+      render :new
+    end
+
   end
 
   def edit
@@ -20,5 +30,11 @@ class NumbersController <ApplicationController
   end
 
   def delete
+  end
+
+  private
+
+  def number_params
+    params.require(:number).permit(:number, :name, :user_id)
   end
 end
