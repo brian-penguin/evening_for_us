d10bbe4a9e911fc3e70588835bb4d2c48af0ab17
brimatteng <btenggren20@gmail.com>
2014-08-14 16:54:58 -0400

    Extracted yelp interaction to it's own model

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,23 +1,12 @@
-# Needs a search result and and a show.
 
 class PlacesController <ApplicationController
-  # Configuring Yelp API is done in the config initializers dir
-  include Yelp::V2::Search::Request
-
   def search
-    @client = Yelp::Client.new
-
+    # gets the search from form params
     term = params[:term]
     city = params[:city]
     state = params[:state]
 
-    #if the user has given all the data then do (all form fiels are required)
-
-    # In the search User will give City, State, and term
-    request = Location.new(city: city, state: state, radius: 2, category:['arts','nightlife'], term: term)
-    # Gets a Hash of businesses (you can only see 20)
-    response = @client.search(request)
-    @places = response['businesses']
+    @places = Search.send_request(city,state,term)
 
   end
 end
